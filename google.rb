aee066b44a44cdb5f282e2b1eb6d7d08e0c35977
pat-thomas <patthomassoftware@gmail.com>
2013-08-11 10:19:42 -0400

    google script is less nosiy

--- google.rb
+++ google.rb
@@ -4,8 +4,6 @@ output = `uname -a`
 output.downcase!
 if output.include?('linux') then
 	system "google-chrome \"http://www.google.com/search?q=#{url_param}\""
-	puts "googling #{url_param}"
 	exit 0
 end
-puts "googling #{url_param}"
 system "open \"http://www.google.com/search?q=#{url_param}\""
