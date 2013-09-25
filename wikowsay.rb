5eb28fdbe79eae78499846219ce38f1bb4facf4d
pat-thomas <patthomassoftware@gmail.com>
2013-09-25 12:59:29 -0400

    exception handling if page does not exist on wikipedia

--- wikowsay.rb
+++ wikowsay.rb
@@ -7,6 +7,14 @@ args = ARGV
 args[0] = args[0].downcase.capitalize
 args[1..-1].map! { |a| a.downcase }
 url_param = args.join('_')
-document = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{url_param}"))
+
+# Catch exception if Wikipedia page doesn't exist
+begin
+  document = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/#{url_param}"))
+rescue
+  system 'cowsay -d "Couldn\'t find it"'
+  exit 0
+end
+
 paragraphs = document.xpath('//p')
 system "cowsay '#{paragraphs[1].content}'"
