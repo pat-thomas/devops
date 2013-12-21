1d4154835a14508c04595c7ea69f5848f35581a8
Pat Thomas <patthomassoftware@gmail.com>
2013-12-21 17:38:21 -0500

    remove unused var

--- install.rb
+++ install.rb
@@ -1,7 +1,6 @@
 # This is a one-off script that should be run to install (or update)
 # the various scripts in this directory. It will symbolically link
 # all the scripts in this directory to ~/usr/bin.
-working_dir = `pwd`
 rb_file_string = `ls *.rb`
 rb_files = rb_file_string.split( /\r?\n/ )
 rb_files.keep_if { |fname| fname != "install.rb" }
