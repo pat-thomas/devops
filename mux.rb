c8954cd33e16da70e7463ff04b20f4e43628d61e
Pat Thomas <pthomas@relaynetwork.com>
2014-01-06 13:39:33 -0500

    mux is smaller and better

--- mux.rb
+++ mux.rb
@@ -1,15 +1,6 @@
 #!/usr/bin/ruby
 # Script for opening tmux session with standard windows
 
-def shell_out_and_exit cmd
-  system "#{cmd}"
-  exit 0
-end
-
-def sessions_exist
-  system "tmux list-sessions > /dev/null 2>&1"
-end
-
 def create_standard_tmux_session session_name
   commands = [
 	  "tmux new -d                         -s #{session_name} -n emacs",
@@ -28,99 +19,85 @@ def create_standard_tmux_session session_name
 	commands.each do |cmd|
 		system cmd
 	end
-end
+end # create_standard_tmux_session
 
 def current_tmux_sessions
 	current_sessions = `tmux list-sessions | cut -f1 -d:`.split("\n")
   current_sessions
-end
-
-def output_current_tmux_sessions
-  local_current_tmux_sessions = current_tmux_sessions
-  if not local_current_tmux_sessions.empty?
-    puts "Found the following sessions:"
-    local_current_tmux_sessions.each do |session|
-      puts "-> #{session}"
-    end
-  end
-	local_current_tmux_sessions
-end
+end # current_tmux_sessions
 
 def pull_off_working_directory
 	working_dir = `pwd`
 	working_dir = working_dir[0..working_dir.length-2].split("\/").last
 	working_dir
-end
+end # pull_off_working_directory
 
-def create_new_session session_name
-	new_session_flag = ''
-	while new_session_flag != 'y' or new_session_flag != 'n'
-		print "Create new tmux session #{session_name}? (y/n) > "
-		new_session_flag = STDIN.gets.chomp
-		if new_session_flag == 'y' then
-			create_standard_tmux_session session_name
-			exit 0
-		elsif new_session_flag == 'n' then
-			puts "exiting..."
-			exit 0
-		end
-	end
-end
-
-new_session = ARGV.first
-# List sessions
-if new_session == 'list' then
-  output_current_tmux_sessions
-  exit 0
-end
-
-# Kill specified session
-if new_session == 'kill'
-  sessions_to_kill = ARGV[1..-1]
-  if not sessions_to_kill.nil?
-    sessions_to_kill.each do |session|
-      system "tmux kill-session -t #{session}"
-    end
-    exit 0
+def list_sessions sessions_to_grep_for
+  if sessions_to_grep_for.empty?
+    puts "{#{current_tmux_sessions.join(',')}}"
   else
-    puts "error: must enter a session name to kill"
-    exit 0
+    puts "{#{(grep_for_sessions sessions_to_grep_for).join(',')}}"
+  end
+end # list_sessions
+
+def grep_for_sessions sessions_to_grep_for
+  accum = []
+  (sessions_to_grep_for.map { |s| s.downcase }).each do |session|
+    current_tmux_sessions.each do |current_session|
+      if current_session.downcase.include? session then
+        accum.push current_session
+      end
+    end
+  end
+  accum
+end # grep_for_sessions
+
+def create_or_join_session session_name
+  if session_name == '--here' then
+    session_name = pull_off_working_directory
+  end
+  existing_session_joined = attempt_to_join_existing_session session_name
+  if not existing_session_joined then
+    new_session_flag = ""
+    while new_session_flag != 'y' or new_session_flag != 'n'
+      print "Create new tmux session #{session_name}? (y/n) > "
+      new_session_flag = STDIN.gets.chomp
+      if new_session_flag == 'y' then
+        create_standard_tmux_session session_name
+        exit 0
+      elsif new_session_flag == 'n' then
+        puts "exiting..."
+        exit 0
+      end
+    end
+  end
+end # create_or_join_session
+
+def attempt_to_join_existing_session session_name
+  current_tmux_sessions.each do |name|
+    if name == session_name then
+      system "tmux attach -t #{session}"
+      return true
+    end
+  end
+  return false
+end # attempt_to_join_existing_session
+
+def kill_sessions session_names
+  session_names.each do |session|
+    system "tmux kill-session -t #{session}"
+  end
+end # kill_sessions
+
+def main
+  case ARGV[0]
+  when "list"
+    list_sessions ARGV[1..-1]
+  when "kill"
+    kill_sessions ARGV[1..-1]
+  else
+    create_or_join_session ARGV[0]
   end
 end
 
-# Create new session with the name of the current directory
-if new_session == '--here' then
-	new_session = pull_off_working_directory
-end
-if new_session == nil then
-	puts "error: must enter a session name"
-	exit 1
-
-else
-
-	if sessions_exist then
-		current_sessions = current_tmux_sessions
-		current_sessions.each do |session|
-			if session == new_session then
-				# Found a session with the same name, prompt to either join the session or exit.
-				join = ''
-				while join != 'y' or join != 'n'
-					print "Found session #{session}, join? (y/n) > "
-					join = STDIN.gets.chomp
-					if join == 'y' then
-						system "tmux attach -t #{session}"
-						exit 0
-					elsif join == 'n' then
-						puts "exiting..."
-						exit 0
-					end
-				end
-			end
-		end
-
-		# Create a new session
-		create_new_session new_session
-	end
-
-	create_new_session new_session
-end
+main
