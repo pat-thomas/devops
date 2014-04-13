0fe60dd867055d18f7201529912c6738ae4b2927
Pat Thomas <patthomassoftware@gmail.com>
2014-04-12 21:16:15 -0400

    check_repos script

---
+++ check_repos.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+require "open3"
+
+@projects_root = "/Users/#{`whoami`}/projects".gsub("\n","")
+
+def is_git_directory? dir
+  git_status = Open3.popen3("git status") { |stdin, stdout, stderr, wait_thr| stdout.read() }
+  !git_status.include? "fatal: Not a git repository (or any of the parent directories): .git"
+end
+
+def repo_is_dirty? dir
+  git_status = Open3.popen3("git status") { |stdin, stdout, stderr, wait_thr| stdout.read() }.split("\n")
+  git_status.keep_if { |line|
+    line == "Changes not staged for commit:" or
+    line == "Untracked files:"  or
+    line == "Changes to be committed:"
+  }.size > 0
+end
+
+def main
+  accum = []
+  Dir.entries(@projects_root).delete_if { |d| d == '.' or d == '..' }.each do |dir|
+    full_path = "#{@projects_root}/#{dir}"
+    Dir.chdir full_path do
+      if is_git_directory? full_path and repo_is_dirty? dir
+        accum.push(full_path)
+      end
+    end
+  end
+
+  if accum.size > 0
+    puts "The following repositories have uncommitted changes:"
+    accum.each do |el|
+      puts el
+    end
+  end
+end
+
+main
