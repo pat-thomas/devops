1d0da86ea9777385d157fb304673352949d1790a
Pat Thomas <pthomas@relaynetwork.com>
2014-08-21 23:06:28 -0400

    Default to using my username to clone repos.

--- clone.rb
+++ clone.rb
@@ -5,6 +5,12 @@ if ARGV.length < 1
 	exit
 end
 
-repo_user = ARGV[0]
-repo_name = ARGV[1]
+if ARGV.length == 1 # assume you want to clone my repo, because I'm awesome
+  repo_user = "pat-thomas"
+  repo_name = ARGV[0]
+else
+  repo_user = ARGV[0]
+  repo_name = ARGV[1]
+end
+
 system "git clone git@github.com:#{repo_user}/#{repo_name}.git"
